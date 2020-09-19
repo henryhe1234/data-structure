b524de954a4774de29e77ed95f2fbd4134f00e0f
henryhe1234 <henryhe1123@gmail.com>
2020-09-19 19:08:57 +0000

    improved version using map for 1512 question

--- programming-problem/leetcode/1512_#of_Good_Pairs.js
+++ programming-problem/leetcode/1512_#of_Good_Pairs.js
@@ -1,13 +1,31 @@
 var numIdenticalPairs = function(nums) {
   let count = 0;
   
-  for(let i =0;i<nums.length;i++){
-      for(let j = i+1;j<nums.length;j++){
-          if(nums[j] === nums[i]){
-              count +=1;
+  for(let i = 0; i < nums.length; i++) {
+      for(let j = i+1; j < nums.length; j++) {
+          if(nums[i] == nums[j]) {
+              count++;
           }
       }
   }
-  return count;
+  
+  return count
+};
+
+var numIdenticalPairs1 = function(nums) {//O(n) using maps
+  let obj = {};
+  let answer = 0;
+  for(let num of nums){
+      if(!obj[num]){
+          obj[num] = 1;
+      }else{
+          obj[num] += 1;
+      }
+  }
+  for(let key in obj){
+      answer += obj[key] * (obj[key] -1) /2;
+  }
+  return answer;
   
 };
+// numIdenticalPairs([1,1,1,1]);
