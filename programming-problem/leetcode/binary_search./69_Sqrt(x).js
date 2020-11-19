6fcf70d5e8b0b783667870253f87fe82c48996bf
henryhe1234 <henryhe1123@gmail.com>
2020-11-19 14:01:05 -0700

    multiple leetcode problem

---
+++ programming-problem/leetcode/binary_search./69_Sqrt(x).js
@@ -0,0 +1,43 @@
+// Implement int sqrt(int x).
+
+// Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+// Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+// Example 1:
+
+// Input: 4
+// Output: 2
+// Example 2:
+
+// Input: 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since 
+//              the decimal part is truncated, 2 is returned.
+
+var mySqrt = function(x) {
+  if(x === 0 || x === 1){
+      return x;
+  }
+  let i = 1; result = 1;
+  while(result <= x){
+      i++;
+      result = i * i;
+  }
+  return i-1
+};
+var mySqrt = function (x) {
+let left = 0;
+let right = Math.ceil(x / 2);
+let mid = 0;
+
+while (left <= right) {
+  mid = Math.floor((left + right) / 2);
+
+  if (mid * mid < x) left = mid + 1;
+  else if (mid * mid > x) right = mid - 1;
+  else return mid;
+}
+
+return right;
+};
