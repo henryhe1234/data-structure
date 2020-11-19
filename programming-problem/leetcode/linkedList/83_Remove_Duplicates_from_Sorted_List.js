d1403ab525ae73b6a6fd26bae9470683cbd986cd
henryhe1234 <henryhe1123@gmail.com>
2020-11-19 14:28:11 -0700

    leetcode question 83

---
+++ programming-problem/leetcode/linkedList/83_Remove_Duplicates_from_Sorted_List.js
@@ -0,0 +1,23 @@
+// Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+// Example 1:
+
+// Input: 1->1->2
+// Output: 1->2
+// Example 2:
+
+// Input: 1->1->2->3->3
+// Output: 1->2->3
+
+var deleteDuplicates = function(head) {
+  let current = head;
+  while(current !== null && current.next !== null){
+      if(current.next.val === current.val){
+          current.next = current.next.next;
+          
+      }else{
+          current = current.next;
+      }
+  }
+  return head;
+};
